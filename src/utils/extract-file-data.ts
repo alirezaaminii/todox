5aedb30915654e4c47c6f8e2de835ec92bf9f659
alireza_amini <aminicss1@gmail.com>
2023-03-18 21:13:33 +0330

    refactor: remove unused utils

--- src/utils/extract-file-data.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-
-export const extractFileData = (filePath: string, defaultStructure: unknown) => {
-  return fs.existsSync(filePath)
-    ? JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-    : defaultStructure
-}
